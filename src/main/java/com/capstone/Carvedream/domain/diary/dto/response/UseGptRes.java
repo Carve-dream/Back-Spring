c0fec6a2f6f085443a0bfb463ada1e27f4e5e2a4
choeun7 <short0720@gmail.com>
2024-04-29 21:47:28 +0900

    Feat: 응답 예시 추가

--- src/main/java/com/capstone/Carvedream/domain/diary/dto/response/UseGptRes.java
+++ src/main/java/com/capstone/Carvedream/domain/diary/dto/response/UseGptRes.java
@@ -1,10 +1,12 @@
 package com.capstone.Carvedream.domain.diary.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UseGptRes {
 
+    @Schema(description = "해몽결과", example = "돼지꿈은 돈을 상징합니다.")
     private String answer;
 
     public UseGptRes(String answer) {
