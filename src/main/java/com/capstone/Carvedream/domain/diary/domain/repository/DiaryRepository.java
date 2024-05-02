6a5c3d2ae67004fffcfeb71138c8057240ffe33f
gr-KIM <ram770281@gmail.com>
2024-05-02 21:33:17 +0900

    Chore: 불필요 코드 삭제

--- src/main/java/com/capstone/Carvedream/domain/diary/domain/repository/DiaryRepository.java
+++ src/main/java/com/capstone/Carvedream/domain/diary/domain/repository/DiaryRepository.java
@@ -1,9 +1,7 @@
 package com.capstone.Carvedream.domain.diary.domain.repository;
 
 import com.capstone.Carvedream.domain.diary.domain.Diary;
-import com.capstone.Carvedream.domain.diary.domain.Emotion;
 import com.capstone.Carvedream.domain.user.domain.User;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,8 +16,6 @@ import java.util.Optional;
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     Page<Diary> findAllByUser(User user, PageRequest pageRequest);
-
-    Integer countByEmotionAndUser(Emotion emotion, User user);
 
     List<Diary> findAllByUserAndDateBetween(User user, LocalDate start, LocalDate end);
 
